bfcbf9e2de2038359f8af2f0acf34546277ee4ef
NiklasSorensen <niklass137@gmail.com>
2017-11-17 15:26:20 +0100

    start funktion til getUsers

--- wot-pi/wot-server.js
+++ wot-pi/wot-server.js
@@ -3,6 +3,7 @@ var httpServer = require('./servers/http'),
     actuators = require('./routes/actuators'),
     request = require('request'),
     bluetoothPlugin = require('./plugins/internal/bluetoothPlugin');
+    ourRequest = require('./communication/request.js');
 
 var server = httpServer.listen(resources.pi.port, function () {
     console.info('Your WoT Pi is up and running on port %s',
@@ -14,6 +15,7 @@ var server = httpServer.listen(resources.pi.port, function () {
 //Når det køres på PI, skal simulate være sat til false
 bluetoothPlugin.start({'simulate': false, 'frequency': 2000});
 
+ourRequest.getUsers();
 
 //onOffLight('http://192.168.0.108/api/zwxLWe5QUN6m3R0F92GoSOdT6rvq0cPw6THRxfJA/lights/1/state',true);
 
