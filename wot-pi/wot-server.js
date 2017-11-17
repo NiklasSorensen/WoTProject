0b2ab683b60eccc5d1a19d8da8efac19f50c0f9d
NiklasSorensen <niklass137@gmail.com>
2017-11-17 15:47:27 +0100

    start funktion til getUsers

--- wot-pi/wot-server.js
+++ wot-pi/wot-server.js
@@ -15,7 +15,7 @@ var server = httpServer.listen(resources.pi.port, function () {
 //Når det køres på PI, skal simulate være sat til false
 bluetoothPlugin.start({'simulate': false, 'frequency': 2000});
 
-//ourRequest.getUsers();
+ourRequest.getUsers();
 
 //onOffLight('http://192.168.0.108/api/zwxLWe5QUN6m3R0F92GoSOdT6rvq0cPw6THRxfJA/lights/1/state',true);
 
